55ed7ef01d9cef6723d26c5cafeaf0a5b20915a9
prahz <prashantiamthebest9@gmail.com>
2026-01-11 15:40:15 +0530

    Made some changes in server.js

--- notes-finder/server.js
+++ notes-finder/server.js
@@ -1,10 +1,25 @@
 const http = require('http');
-const path = require('path');
-const    server = http.createServer((req,res) => {
-    res.write('hello world');
-    res.end();
-    console.log(req.method);
-    console.log(req.url);
+const server = http.createServer((req, res) => {
+  const myUrl = new URL(req.url, 'http://localhost:3000');
+  const pathname = myUrl.pathname;
+  const checkIn = myUrl.searchParams.get('check-in');
+
+  if (pathname === '/health' && checkIn === 'yes' && req.method === 'GET') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.write(JSON.stringify({ message: 'you are on check-in page' }));
+
+  } else if (pathname === '/health' && req.method === 'GET') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.write(JSON.stringify({ message: 'you are on health' }));
+
+  } else if (pathname === '/' && req.method === 'GET') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.write(JSON.stringify({ message: 'you are on root' }));
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.write('Error 404 Not Found');
+  }
+
+  res.end();
 });
 server.listen(3000);
- 

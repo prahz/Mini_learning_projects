5af52c6e567db20bfe5c20bf398cbde3c5a4849e
prahz <prashantiamthebest9@gmail.com>
2026-01-19 13:57:51 +0530

    made a small change in notes.router

--- notes-finder/routes/notes.routes.js
+++ notes-finder/routes/notes.routes.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const {getNotes,updateNote,deleteNote,createNote} = require('../controllers/notes.controller');
 router.get('/', getNotes);
 router.post('/',createNote);
-router.put('/',updateNote);
-router.delete('/',deleteNote);
+router.put('/:id',updateNote);
+router.delete('/:id',deleteNote);
 module.exports = router;

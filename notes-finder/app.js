6fd11c6cc82287dd9a1823328f143c565c726d2c
prahz <prashantiamthebest9@gmail.com>
2026-01-25 15:47:38 +0530

    minor change

--- notes-finder/app.js
+++ notes-finder/app.js
@@ -3,7 +3,7 @@ const port = 3000;
 const app = express();
 
 const notesRouter = require('./routes/notes.routes.js');
-
+const errorHandler = require('./middleswares/error.middleware.js');
 app.use(express.json()); // middlware to give req.body its value
 app.use('/notes', notesRouter);
 app.use(errorHandler);
